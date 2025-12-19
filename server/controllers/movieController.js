2a8ba2f16d7859251f6af8386bf7af278a482926
felix <felixperez233@gmail.com>
2025-12-19 16:07:28 -0500

    add movie controller and route. Fixed a mix-up where I confused route with model

--- server/controllers/movieController.js
+++ server/controllers/movieController.js
@@ -3,9 +3,8 @@ import {
     getMovieByIdFromDB,
     createMovieInDB,
     deleteMovieFromDB,
-    updateMovieInDBdateMovieInDB,
-    createMovieInDB,
-} from "../routes/movie";
+    updateMovieInDB,
+} from "../models/movieModel";
 
 export const getAllMovies = async (_req, res) => {
     try{
@@ -19,7 +18,7 @@ export const getAllMovies = async (_req, res) => {
 
 export const getMovieById = async (req, res) => {
     try{
-        const { id } = req.params
+        const { id } = req.params;
         const movie = await getMovieByIdFromDB(id);
         if (!movie){
             return res.status(404).json({error: 'Movie not found.'})
@@ -38,10 +37,44 @@ export const createMovie = async (req, res) => {
             return res.status(400).json({error: 'Missing requeired field: title, description, release_year, poster_url.'})
         }
 
-        const movie = await createMovieInDB({title, description, release_year, poster_url})
+        const movie = await createMovieInDB([title, description, release_year, poster_url])
         res.status(201).json(movie)
     } catch (error) {
         console.log("Database Error: ", error.message);
         res.status(500).json({error: "Error has occured creating movie."});
     }
+};
+
+export const updateMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, description, release_year, poster_url } = req.body;
+        
+        const movie = await updateMovieInDB(id, {title, description, release_year, poster_url});
+        
+        if (!movie) {
+            return res.status(404).json({ error: 'Movie not found' });
+        }
+        
+        res.json(movie);
+        } catch (error) {
+            console.log("Database Error: ", error.message);
+            res.status(500).json({ error: "Error has occured updating movie." });
+        }
+};
+
+export const deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleted = await deleteMovieFromDB(id);
+        
+        if (!deleted) {
+          return res.status(404).json({ error: 'Movie not found' });
+        }
+
+        res.json({ message: 'Movie deleted successfully' });
+        } catch (error) {
+            console.log("Database Error: ", error.message);
+            res.status(500).json({ error: "Error occured trying to delete movie." });
+        }
 };
