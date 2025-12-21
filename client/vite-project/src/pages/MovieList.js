cea4a2432708110924164054494541b0084ca6da
felix <felixperez233@gmail.com>
2025-12-20 20:41:20 -0500

    finished movieList.js

--- client/vite-project/src/pages/MovieList.js
+++ client/vite-project/src/pages/MovieList.js
@@ -4,7 +4,61 @@ import MovieCard from '../components/MovieCard';
 
 
 const MovieList = () => {
-    const [movie, serMovie] = useState([]);
+    const [movies, setMovies] = useState([]);
     const [loading, setloading] = useState(true);
     const [error, setError] = useStae(null);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try{
+                setloading(true);
+                const data = await getAllMovies();
+                setMovies(data);
+                setError(null);
+            } catch (error) {
+                setError('Failed to load movies');
+                console.error('Error fetching movies: ', error);
+            } finally {
+                setloading(false);
+            }
+        };
+
+        fetchMovies();
+    }, []);
+
+    if (loading){
+        return(
+            <div className="movie-list-container">
+                <h1>Movie Reviews</h1>
+                <p>Loading movies...</p>
+            </div>
+        );
+    }
+
+    if (error) {
+        return(
+            <div className="movie-list-container">
+                <h1>Movie Reviews</h1>
+                <p>{error}</p>
+            </div>
+        );
+    }
+
+    return(
+        <div className="movie-list-container">
+            <h1>Movie Reviews</h1>
+            {movies.length === 0 ? (
+                <p>No movies found.</p>
+            ) : (
+                <div className="movie-grid">
+                    {movies.map((movie) => (
+                        <MovieCard key={movie.id} movie={movie} />
+                    ))}
+                </div>
+                )
+            }
+        </div>
+    );
 };
+
+export default MovieList;
