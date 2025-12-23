bb3815e4c60d4e66e054a7c3e12fd420fb8531c1
felix <felixperez233@gmail.com>
2025-12-22 22:19:02 -0500

    made reverted some changes to run it locally again, commented out a few lines of code

--- server/server.js
+++ server/server.js
@@ -4,14 +4,15 @@ import dotenv from 'dotenv';
 import reviewRoutes from './routes/reviews.js';
 import movieRoutes from './routes/movies.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
+//import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-//const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.resolve();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.resolve();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 // 1. General Middleware
 app.use(cors());
@@ -21,7 +22,7 @@ app.use(express.json());
 app.use('/api/movies', movieRoutes);
 app.use('/api/reviews', reviewRoutes);
 
-/*
+
 app.get('/', (req, res) => { 
   res.status(200).send('Movie Review API Server is Operational. Use /api/movies and /api/reviews endpoints.');
 });
@@ -29,28 +30,31 @@ app.get('/', (req, res) => {
 // Health check
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
-}); */
+});
 
 // Health check
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
 });
 
-// 3. Static Files
-const distPath = path.join(__dirname, '../client/vite-project/dist');
-app.use(express.static(distPath));
 
+// 3. Static Files
+// const distPath = path.join(__dirname, '../client/vite-project/dist');
+// app.use(express.static(distPath));
+
+/*
 // 4. The Catch-all 
 app.get('*', (req, res) => {
   res.sendFile(path.join(distPath, 'index.html'));
 });
+*/
 
 // 5. Port Listening (Only if not on Vercel/Production)
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => {
+//if (process.env.NODE_ENV !== 'production') {
+//    const PORT = process.env.PORT || 5000;
+   app.listen(PORT, () => {
         console.log(`Server is running on port http://localhost:${PORT}`);
     });
-}
+// }
 
 export default app;
